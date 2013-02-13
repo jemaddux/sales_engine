0a76a427ff6b3fa7aa3ab8b4336cca8e7e628f81
John Maddux <jemaddux@gmail.com>
2013-02-13 15:27:11 -0700

    bump

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,4 +2,18 @@ require './lib/get_csv_mod'
 
 class Customer
 	extend GetCSV
+
+
+  attr_accessor :name, :id
+
+  def self.make_customers
+  	#runs through each customers from the get_csv and sends the to initialize
+  	#adds each customer to a @list_of_customers which is an array
+  	#this will run from sales_engine.rb
+  end
+
+  def initialize()#takes in a hash
+  	@id = input[:id]
+  	#returns a customer instance
+  end
 end
