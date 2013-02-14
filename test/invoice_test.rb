92e1961ef66789bd0e4854086e4f806b1783be64
Kyle Suss <susskyle@gmail.com>
2013-02-13 18:46:27 -0700

    copied over tests and methods for instance class

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -3,7 +3,7 @@ require "./lib/invoice"
 
 class InvoiceTest < MiniTest::Unit::TestCase
 	def test_it_exists
-		invoice = Invoice.new
+		invoice = Invoice.new({})
 		assert_kind_of Invoice, invoice
 	end
 
@@ -12,4 +12,45 @@ class InvoiceTest < MiniTest::Unit::TestCase
 		file_contents = Invoice.get_csv(invoice_file)
 		assert_operator file_contents.size, :>= , 5
 	end
+
+# copy/paste starts here
+
+  def test_that_we_can_get_an_invoice_id_from_an_instance
+    invoice = Invoice.new(:id => "1")
+    assert_equal "1", invoice.id
+  end
+
+  def test_that_we_can_get_an_invoice_customer_id_from_an_instance
+    invoice = Invoice.new(:customer_id => "3")
+    assert_equal "3", invoice.customer_id
+  end
+
+  def test_that_we_can_get_an_invoice_created_date_from_an_instance
+    time = Time.now
+    invoice = Invoice.new(:created_at => time)
+    assert_equal time, invoice.created_at
+  end
+
+  def test_that_we_can_get_an_invoice_updated_date_from_an_instance
+    time = Time.now
+    invoice = Invoice.new(:updated_at => time)
+    assert_equal time, invoice.updated_at
+  end
+
+  def test_that_we_can_create_invoice_objects_using_simulated_data
+    single_invoice = {
+      :id => "1",
+      :customer_id => "3",
+      :merchant_id => "25",
+      :status => "shipped",
+      :created_at => "2012-03-25 09:54:09 UTC",
+      :updated_at => "2012-03-27 14:53:59 UTC"
+    }
+    assert_kind_of Invoice, Invoice.new(single_invoice)
+  end
+
+  def test_that_calling_new_invoices_returns_an_array
+    invoice_list = Invoice.make_invoices(true)
+    assert_equal Array, invoice_list.class
+  end
 end
